b6399eca15614266f2668a6f5e85fa62c2254d89
larinegit <larineomoit04@gmail.com>
2024-10-03 14:38:03 +0300

    js and css front end

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to STAFFHUB</h1>
+      <p>Your Simple Employee Information Management System</p>
+      <button>Learn More</button>
+    </div>
+  );
+}
+
+export default App;
